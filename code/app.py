077ee475076968fd776cdfc5ff8385cd1432ea35
beomseong kim <qse55526@gmail.com>
2024-11-10 22:11:06 -0600

    Initial streamlit page setup (have to connect XGBoost and DQN agent)

---
+++ code/app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from stable_baselines3 import DQN
+
+# model = joblib.load("churn_prediction_model.pkl")
+# agent = DQN.load("dqn_churn_agent_model")
+
+st.title("Bank Customer Churn Prediction & Action Recommendation")
+
+gender = st.selectbox("Gender", ["Female", "Male"])
+geography = st.selectbox("Geography", ["France", "Germany", "Spain"])
+age = st.slider("Age", 18, 92, 38)
+is_active_member = st.selectbox("Is an Active Bank Member?", ["Yes", "No"])
+has_credit_card = st.selectbox("Has an Active Credit Card?", ["Yes", "No"])
+card_type = st.selectbox("Card Type", ["DIAMOND", "GOLD", "PLATINUM", "SILVER"])
+num_of_products = st.selectbox("Number of Products", [1, 2, 3, 4])
+credit_score = st.slider("Credit Score", 350, 850, 650)
+tenure = st.slider("Years as Bank Customer (Tenure)", 0, 10, 5)
+point_earned = st.slider("Point Earned", 119, 1000, 600)
+balance = st.number_input("Account Balance", min_value=0, value=100000, max_value= 255000)
+estimated_salary = st.number_input("Estimated Salary", min_value=0, max_value=200000, value = 100000)
+complain = st.selectbox("Complain", ["Yes", "No"])
+
+satisfaction_score = st.selectbox("Satisfaction Score", [1, 2, 3, 4, 5])
+
+if st.button("Predict Churn Probability & Recommend Action"):
+    input_data = pd.DataFrame({
+        'CreditScore': [credit_score],
+        'Age': [age],
+        'Tenure': [tenure],
+        'Balance': [balance],
+        'HasCrCard': [1 if has_credit_card == "Yes" else 0],
+        'IsActiveMember': [1 if is_active_member == "Yes" else 0],
+        'EstimatedSalary': [estimated_salary],
+        'Complain': [1 if complain == "Yes" else 0],
+        'Point Earned': [point_earned],
+        'Geography': [geography],
+        'Gender': [gender],
+        'Card Type': [card_type],
+        'NumOfProducts': [num_of_products],
+        'Satisfaction Score': [satisfaction_score]
+    })
+
+    churn_probability = model.predict_proba(input_data)[0][1]
+    st.write(f"Predicted Churn Probability: {churn_probability:.2%}")
+
+
+    obs = input_data.values
+    action, _ = agent.predict(obs)
+    action_text = agent.get_env().actions[action]
+    st.write(f"Recommended Action: {action_text}")
